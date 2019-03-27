8d41765e213c4f8af87fdf1cf14003423013b487
Halimah <haleemaholadosu@gmail.com>
2019-03-27 17:17:35 +0100

    [Feature #164891027] Create landing page

---
+++ UI/js/main.js
@@ -0,0 +1,9 @@
+const toggleSidebar = () => {
+    const sideba = document.getElementById('sidebar');
+    if (sideba.style.display === 'none') {
+      sideba.style.display = 'block';
+    } else {
+      sideba.style.display = 'none';
+    }
+  };
+  
