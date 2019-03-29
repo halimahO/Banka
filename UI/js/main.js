383efe3b47d465f79ff1d0e3e1d6f1d0a54fc977
Halimah <haleemaholadosu@gmail.com>
2019-03-29 15:15:52 +0100

    [Feature #164892152] Create a view for admin/staff to delete account

--- UI/js/main.js
+++ UI/js/main.js
@@ -23,23 +23,41 @@ const toggleSidebar = () => {
     const modal = document.getElementsByClassName('modal3')[0];
       modal.style.display = 'block';
   };
+  const openModal4 = () => {
+    const modal = document.getElementsByClassName('modal4')[0];
+      modal.style.display = 'block';
+  };
+  const openModal5 = () => {
+    const modal = document.getElementsByClassName('modal5')[0];
+      modal.style.display = 'block';
+  };
 
   const closeModal = () => {
     const modal = document.getElementsByClassName('modal')[0];
     const modal1 = document.getElementsByClassName('modal1')[0];
     const modal2 = document.getElementsByClassName('modal2')[0];
     const modal3 = document.getElementsByClassName('modal3')[0];
+    const modal4 = document.getElementsByClassName('modal4')[0];
+    const modal5 = document.getElementsByClassName('modal5')[0];
+
       modal.style.display = 'none';
       modal1.style.display = 'none';
       modal2.style.display = 'none';
       modal3.style.display = 'none';
+      modal4.style.display = 'none';
+      modal5.style.display = 'none';
+      
   };
   const closeModalStaff = () => {
     const modal = document.getElementsByClassName('modal')[0];
     const modal2 = document.getElementsByClassName('modal2')[0];
     const modal3 = document.getElementsByClassName('modal3')[0];
+    const modal4 = document.getElementsByClassName('modal4')[0];
+    const modal5 = document.getElementsByClassName('modal5')[0];
       modal.style.display = 'none';
       modal2.style.display = 'none';
       modal3.style.display = 'none';
+      modal4.style.display = 'none';
+      modal5.style.display = 'none';
   };
    
