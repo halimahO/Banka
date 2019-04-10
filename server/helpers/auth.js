3fe02afb4680f87adf1e87d1858542ede85056c7
Halimah <haleemaholadosu@gmail.com>
2019-04-10 13:31:55 +0100

    feature(api):Create api endpoint for sigin [Start #165170024]

--- server/helpers/auth.js
+++ server/helpers/auth.js
@@ -3,18 +3,15 @@ import jwt from 'jsonwebtoken';
 
 export const hashPassword = password => bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 
-export const comparePassword = (hash, password) => bcrypt.compareSync(password, hash);
+export const comparePassword = (hash, password) => bcrypt.compareSync(hash, password);
 
 
 const secretKey = 'secretkey';
-export const generateToken = (id) => {	
-	return jwt.sign({userId: id,}, secretKey, { expiresIn: '1week' });
+export const generateToken = (payload) => {	
+	return jwt.sign(payload, secretKey, { expiresIn: '1week' });
 };
 
 export const verifyToken = (token) => {
-    try {
-      return jwt.verify(token, secretKey);
-    } catch (e) {
-      return { error: `${e}` };
-    }
+      const decoded = jwt.verify(token, secretKey);
+      return decoded
   }
