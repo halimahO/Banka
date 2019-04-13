f9f568297f317298efb7f408506bd1d1256e36e7
Halimah <haleemaholadosu@gmail.com>
2019-04-13 08:01:52 +0100

    chore(validation):Implement login validation [Start #165319470]

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import userController from '../controllers/userController';
 import { requireAuth, adminAuth } from '../middlewares/authentication';
+import userValidate from '../middlewares/validateUser';
 
 
 const userRouter = new Router();
 const { createUser, signin, createStaff } = userController;
 
 userRouter.post('/auth/signup', createUser);
-userRouter.post('/auth/signin', signin);
+userRouter.post('/auth/signin', userValidate.login, signin);
 userRouter.post('/staff', requireAuth, adminAuth, createStaff);
 
 export default userRouter;
