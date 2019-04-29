4c4faae45fc77b60d46dddea4686a9f95705825f
Halimah <haleemaholadosu@gmail.com>
2019-04-29 06:07:39 +0100

    Update gh-pages
    Update gh-pages with recent changes

--- UI/js/main.js
+++ UI/js/main.js
@@ -8,98 +8,6 @@ const toggleSidebar = () => {
   }
 };
 
-const openModal = () => {
-  const modal = document.getElementsByClassName('modal')[0];
-  modal.style.display = 'block';
-};
-const openModal1 = () => {
-  const modal = document.getElementsByClassName('modal1')[0];
-  modal.style.display = 'block';
-};
-const openModal2 = () => {
-  const modal = document.getElementsByClassName('modal2')[0];
-  modal.style.display = 'block';
-};
-const openModal3 = () => {
-  const modal = document.getElementsByClassName('modal3')[0];
-  modal.style.display = 'block';
-};
-const openModal4 = () => {
-  const modal = document.getElementsByClassName('modal4')[0];
-  modal.style.display = 'block';
-};
-const openModal5 = () => {
-  const modal = document.getElementsByClassName('modal5')[0];
-  modal.style.display = 'block';
-};
-const openModal6 = () => {
-  const modal = document.getElementsByClassName('modal6')[0];
-  modal.style.display = 'block';
-};
-const openModal7 = () => {
-  const modal = document.getElementsByClassName('modal7')[0];
-  modal.style.display = 'block';
-};
-const openModal8 = () => {
-  const modal = document.getElementsByClassName('modal8')[0];
-  modal.style.display = 'block';
-};
-const openModal9 = () => {
-  const modal = document.getElementsByClassName('modal9')[0];
-  modal.style.display = 'block';
-};
-const openModal0 = () => {
-  const modal = document.getElementsByClassName('modal0')[0];
-  modal.style.display = 'block';
-};
-const openModall = () => {
-  const modal = document.getElementsByClassName('modall')[0];
-  modal.style.display = 'block';
-};
-const closeModal = () => {
-  const modal = document.getElementsByClassName('modal')[0];
-  const modal1 = document.getElementsByClassName('modal1')[0];
-  const modal2 = document.getElementsByClassName('modal2')[0];
-  const modal3 = document.getElementsByClassName('modal3')[0];
-  const modal4 = document.getElementsByClassName('modal4')[0];
-  const modal5 = document.getElementsByClassName('modal5')[0];
-  const modal0 = document.getElementsByClassName('modal0')[0];
-  const modall = document.getElementsByClassName('modall')[0];
-
-  modal.style.display = 'none';
-  modal1.style.display = 'none';
-  modal2.style.display = 'none';
-  modal3.style.display = 'none';
-  modal4.style.display = 'none';
-  modal5.style.display = 'none';
-  modal0.style.display = 'none';
-  modall.style.display = 'none';
-};
-const closeModalStaff = () => {
-  const modal = document.getElementsByClassName('modal')[0];
-  const modal2 = document.getElementsByClassName('modal2')[0];
-  const modal3 = document.getElementsByClassName('modal3')[0];
-  const modal4 = document.getElementsByClassName('modal4')[0];
-  const modal5 = document.getElementsByClassName('modal5')[0];
-  const modal6 = document.getElementsByClassName('modal6')[0];
-  const modal7 = document.getElementsByClassName('modal7')[0];
-  const modal8 = document.getElementsByClassName('modal8')[0];
-  const modal9 = document.getElementsByClassName('modal9')[0];
-  modal.style.display = 'none';
-  modal2.style.display = 'none';
-  modal3.style.display = 'none';
-  modal4.style.display = 'none';
-  modal5.style.display = 'none';
-  modal6.style.display = 'none';
-  modal7.style.display = 'none';
-  modal8.style.display = 'none';
-  modal9.style.display = 'none';
-};
-const closeModalClient = () => {
-  const modal = document.getElementsByClassName('modal')[0];
-  modal.style.display = 'none';
-};
-
 const toggleActiveDeactive = () => {
   const btn = document.getElementById('btn-act');
   if (btn.innerHTML === 'ACTIVATE') {
@@ -126,6 +34,16 @@ Array.from(lists).forEach((listItem) => {
     const tartgetSectionId = event.target.dataset.menu;
     removeClass();
     showCurrentMenu(tartgetSectionId);
-    document.getElementById('menuTopic').innerHTML = tartgetSectionId;
+    document.getElementById('menuTopic').innerHTML = tartgetSectionId.replace(/-/g, ' ');
   });
 });
+
+function goToDashboard() {
+  const email = document.getElementById('emailInput').value;
+  if (email === 'admin@banka.com') {
+    window.location = 'dashboard-admin.html';
+  } else if (email === 'staff@banka.com') {
+    window.location = 'dashboard-staff.html';
+  } else { window.location = 'dashboard.html'; }
+  return false;
+}
