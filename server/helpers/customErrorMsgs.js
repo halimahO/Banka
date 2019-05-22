08089b5998d1b00a001b1194a56b0722570dbf2c
Kafilat Abdulwahab <hadeneekeh01@gmail.com>
2019-05-22 22:11:15 +0100

    chore(Form validation): Improve form validation [Fixes  #166178482]

--- server/helpers/customErrorMsgs.js
+++ server/helpers/customErrorMsgs.js
@@ -1,13 +1,13 @@
 const customErrorMessages = {
-  numeric: ':attribute should contain only numbers.',
+  numeric: 'Please input only numbers for :attribute.',
   digits: ':attribute should be :digits digits long.',
-  min: ':attribute should not be less than :min.',
-  max: ':attribute should not be more than :max.',
-  required: ':attribute is required',
-  email: ':attribute should be a valid email address',
-  alpha_dash: ':attribute should contain only aplphabets, numbers, underscores and dashes',
-  alpha: ':attribute should contain only letters.',
-  boolean: ':attribute should be boolean: true or false.',
+  min: 'The :attribute should not be less than :min characters.',
+  max: 'The :attribute should not be more than :max characters.',
+  required: ':attribute can not be empty',
+  email: 'Please input a valid email address',
+  alpha_dash: ':attribute can only contain any of the following: aplphabets, numbers, underscores and dashes',
+  alpha: 'Please input only letters for :attribute.',
+  boolean: ':attribute should be true or false.',
   in: ':attribute should be one of the following: :in.',
 };
 
