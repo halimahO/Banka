e14fafb21c24cdc54f163073d675ddaf728a268d
chuxmykel <ngwobiachukwudi@gmail.com>
2019-05-23 14:13:19 +0100

    bug(account number): fix account number generator
    - rewrite the account number generator function to generate 10 digit account numbers
    
    [Delivers #166210510]

--- server/helpers/generateAcctNo.js
+++ server/helpers/generateAcctNo.js
@@ -1,3 +1,15 @@
-const generateAcctNo = () => Math.floor(Math.random() * 10000000000);
+const generateAcctNo = () => {
+  let numberString = '';
+
+  while (numberString.length < 10) {
+    numberString += Math.floor(Math.random() * 10);
+  }
+
+  if (numberString.charAt(0) === '0') {
+    numberString += Math.floor(Math.random() * 10);
+  }
+
+  return parseInt(numberString, 10);
+};
 
 export default generateAcctNo;
