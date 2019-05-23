05869a6e722a09983322650af78bbea6cc79e0a7
Adeola Adeyemo <adeoladeyemo@gmail.com>
2019-05-23 21:59:00 +0100

    ft(transfer): implement transfer feature user can transfer funds to another banka account

--- server/database/index.js
+++ server/database/index.js
@@ -3,9 +3,17 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connectionString = process.env.DATABASE_URL;
+// const connectionString = process.env.DATABASE_URL;
 
-const pool = new Pool({ connectionString });
+const obj = {
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.DBPORT,
+  host: process.env.HOST,
+};
+const pool = new Pool(obj);
+// const pool = new Pool({ connectionString });
 
 pool.on('connect', () => {
   console.log('connected to the db');
